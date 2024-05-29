f864bebf2855a62f83132c4a2adac96760117769
Keitu <125948549+Keitu7@users.noreply.github.com>
2024-05-29 14:38:30 +0200

    Essaypasted

--- src/pagesComponents/About.js
+++ src/pagesComponents/About.js
@@ -45,6 +45,99 @@ function About() {
 
                     </p>
                 </div>
+
+                <h3>THEORY</h3>
+                <div className='theorybkImage'></div>
+                <div className='theoryborder'>
+                    <p className='design1'>The design process for the website was a unique journey, espcially considering the new library we had learnt this semester.
+                    I had to find the best suited theme to potray my new learnt skills and found that designing a portfolio would be better suited for me as learning and implementing all the learn knowledge of react was fairly overwhelming for me.
+                    Therefore, designing something familiar was a rather 'safer'option.
+                    Below are the wireframes, colour scheme and inspiration images/references.</p>
+                    <div className='t1'></div>
+                    <div className='t2'></div>
+                    <p className='theory1'>
+                    Internet Art Rationale and Reflection:
+                    The colour palette explored and used throughout this website design makes use of tones of yellow, green, and a darker shade of blue. 
+                    This choice of colour scheme was an expression of my interest in more natural, calmer tones that spoke of a softer touch for the eye at first glance. 
+                    This site is a portfolio site which would mean essentially communicating who the designer is, my persona, and expression of personality. 
+                    The layout simply aims to guide the viewer through an uncomplicated user interface that represents and directly communicates my style of simplicity. 
+                    Inspired by a portfolio site by Shinya Okano, Shinya uses a simple interface that creates and allows for an uncomplicated smooth user experience of the interface elements throughout the site 
+                    – this helped me understand how to clearly communicate the simple User Interface that I wished to create without having bore a viewer with a too simplistic experience. 
+                    The layout is therefore straightforward, the website guides the user from the Homepage (white introduces the site designer) to the About page which leads to the design, theory, and internet art section, and lastly the Contact page. 
+                    The navigation bar and section links provide an easier lead through the page, and the site logo plays the Homepage link. The Font used also tries/aims to communicate the simple, calm, yet still interesting, eye-catching, 
+                    sophisticated natural organic theme the images, and colour scheme communicate. 
+                    </p>
+                    <p className='theory2'>
+                    Dev Blog:
+                    The colour palette explored and used throughout this website design makes use of tones of yellow, green, and a darker shade of blue. 
+                    This choice of colour scheme was an expression of my interest in more natural, calmer tones that spoke of a softer touch for the eye at first glance. 
+                    This site is a portfolio site which would mean essentially communicating who the designer is, my persona, and expression of personality. 
+                    The layout simply aims to guide the viewer through an uncomplicated user interface that represents and directly communicates my style of simplicity. 
+                    Inspired by a portfolio site by Shinya Okano, Shinya uses a simple interface that creates and allows for an uncomplicated smooth user experience of the interface elements throughout the site 
+                    – this helped me understand how to clearly communicate the simple User Interface that I wished to create without having bore a viewer with a too simplistic experience. 
+                    The layout is therefore straightforward, the website guides the user from the Homepage (white introduces the site designer) to the About page which leads to the design, theory, and internet art section, and lastly the Contact page. 
+                    The navigation bar and section links provide an easier lead through the page, and the site logo plays the Homepage link. The Font used also tries/aims to communicate the simple, calm, yet still interesting, eye-catching, 
+                    sophisticated natural organic theme the images, and colour scheme communicate. 
+                    </p>
+                </div>
+
+                <h4>ESSAY</h4>
+                <div className='essaybkImage'></div>
+                <div className='essayborder'>
+                <p className='essay1'>
+                Net/Web Art: Creating Something to Appreciate
+                </p>
+                <p className='essay2'>
+                Web designers create work worth interacting with. Although the User Interface and User Experience can be artistic and beautifully designed, web designers create something to use, while Net Art is about creating art for appreciation – 
+                artists create something to be appreciated for its sentimental beauty and emotional power (Sharp, 2022). Net/ Web Art concerns itself with using the internet as its medium and platform, to create, collaborate and distribute its artistic practices. 
+                Therefore, net/ web art is not limited to only interactive websites but includes multiple mediums that collaborate to use the internet as their canvas and gallery to create artworks – creating participatory artwork. (introduce your chosen artwork) 
+                The Net/Web art example that we will examine in this essay is a piece by Olia Lialina called ‘My Boyfriend Came Back from the War’, 1996. This browser-art piece is one of unique mediums and materials, all of which this essay will examine. 
+                </p>
+                <p className='essay3'>
+                This essay will critically analyse and focus on discussing a piece of net/web art. The analyses will discuss the artwork’s 
+                resonation with my chosen area of interest on its conceptual, aesthetic, and technical levels. This essay will begin by briefly defining the term net/web art, 
+                how it is created, and examples of the term. In providing a description context of the artwork, the selected artwork example, the essay will present a detailed description of the art piece, together with its visual,
+                 and interactive elements, its creation context, and cultural and historical background. Thereafter, an analysis of the conceptual aspects of the artwork, considering its themes, visual design, and aesthetic qualities will help evaluate the artwork’s 
+                 overall impact and effectiveness. The essay will then study and reflect on how the internet is/can be used as an art medium, and by incorporating theoretical frameworks from provided readings to substantiate the reflection. Moreover, understanding of the 
+                 Internet as an art medium/platform will be corroborated by the frameworks/research drawn from the provided readings by Aria Dean, 10 Artistic Masterpieces Meant to be Experienced Online; Agents of Change: The Internet. Net Art and How The World Wide Web Has Created A New Medium by Benedetta 
+                 Ricci and ‘The New Museum Hits “Save” on Net Art’ by Frani O’Toole. 
+                </p>
+                <p className='essay4'>
+                Delving into a deeper conversation with what the term net art means, the word refers to a genre that uses the internet medium/ platform as its canvas – it is art made on the internet, and for the internet. Net art entails a combination/collaboration of various sub-genres of computer-based elements such as browser and software art (Tate). 
+                Net art explores network art created since the late 1970s. 
+                Inspired by the works of artists like Eduardo Kac (‘Reabracadabra,’ 1985, Visual poem presented on the early telecommunication network Videotext (Chatel, 2019) see Figure 1.1) and Olia Lialina, use of pre-internet materials and telecommunication networks were used to create interactive works. 
+                The term ‘net art’ references art in the 1990s, referred to as “Web 1.0” (Chatel, 2019) described by its static website pages (Chatel, 2019) as well as network art which practices telecommunication of interactive works. Later, in the mid-2000s, post-internet art emerged focusing on creating art outside the 
+                internet – enabling real-time sharing and various artistic interfaces. Through time, together these elements enable what net art today, allows. Net art includes the fusion of interactive works, telecommunication, “real-time content sharing and dynamic interfaces” (Chatel, DANAE, 2019), and new aesthetics such as virtual worlds and digital technology. 
+                Although Chatel relates the term net art to a more encompassing relation to “network art” (Chatel, 2019), and net art concerns itself with the internet as its canvas, I believe using telecommunication networking elements with materials of software and browser art, the term can stand for both networking/ internet art(work). 
+
+                </p>
+                <p className='essay5'>
+                     In examining an example of the concept of net art, the chosen art piece is a browser-based artwork created by Russian artist Olia Lialina (see Figure 1.2). The artist, film and new media critic Lialina uses the web to interrogate and challenge our perception of sequential storytelling as well as the “organization of memory” (jcook19, 2019). 
+                     In ‘My Boyfriend Came Back from the War’, 1996 Lialina uses interconnected browser and software art elements and materials to create interactive viewing experiences. The story is about a narrative of two individuals, a girlfriend and a boyfriend as lovers. 
+                     The two lovers reunite after the boyfriend returns from the war. The story uses HTML elements to tell the story interactively and engagingly. The art piece uses, what seems like old still pictures and a multilinear structure where the audience can press fragments of the screen which split in half with each given press. 
+                     Two choices appear and wait on what we want to do in that specific situation. Allowing for the extension of two choices in each situation, the work comprises multiple black-and-white marked hyperlinked frames of images and texts (fragments) through which participants can navigate into different non-linear narrative pathways of the story – much like today’s Twine (software). 
+                     Lialina says the reason for the interactive hypertext form of narrative storytelling is that something concerning the medium of the net, should include elements of the net, otherwise how much of a net art is it – “If something is in the net, it should speak in net language” (Lialina in Artsy). 
+                     The artwork has inspired great tribute artworks and provides a space in which the piece can be understood/ engaged with universally by being open to imagination. The net art uses audiences of the internet/ networking telecommunicators as the interactors of the artwork, and Lialina’s art piece uses elements of the internet to make engageable internet art. 
+                     Vuk Cosic has described that art on the net as being nothing more than “documentation of art, which is not created on the net, but rather outside it and, in terms of content…”(Vuk Cosic in Ricci, 2020), net art concerns itself with work needing the net to function, just as Lialina has used HTML, hyperlinks and browser art elements to create the work. 
+                </p>
+                <p className='essay6'>
+                    How this artwork concerns itself with my chosen studies of interest: Although an older piece, the artwork uses website design features such as web technologies, interactive features, and a simple, but effective user interface, it relates to my chosen field of interest in its emphasized practice of interactivity.
+                     ‘My Boyfriend Came Back from the War’ uses hyperlinked images and text to engage the audience and therefore, evoking some sort of emotional investment into the story narrative. The choice of participants having the final say in which way the narrative goes pushes the participants to use their imagination. 
+                     The audience already having to invest in/rely on their imagination for them to continue further engaging with the artwork makes this piece an interactively interesting piece to consume. My specific area of interest in studying interactive media/web design is in using the internet, browser, 
+                     and software elements and my skill to create work that engages and evokes emotion in the viewer, whether it be pleasant or ‘fun’. Creating websites that have User Interface and User Experience that leave the viewer with some sort of memorable experience would be an interesting study. 
+                     Evoking emotion and memorable experiences whether through sensory, auditory or touch interactivity through creative, yet simple, good design is what I eventually aim to achieve. This net art piece by Lialina has shown how using elements of the net to create an artistic piece, although web design does not aim specifically to create art, 
+                     can be used creatively to evoke emotion and engage creativity from the user. Likewise, I can learn to create/use net elements related to web design to help make the user feel something, no matter how simple – awakening focused interest from the user into the website experience can evoke genuine interest. 
+                    Perhaps by focusing on interactive elements such as the hyperlinked components used in ‘My Boyfriend Came Back from the War’, making use of a well-crafted interactive user interface may be a way to begin. 
+                </p>
+                <p className='essay7'>
+                In concluding this piece, we have come to understand how the term net/web art much or much not relates to the art of the internet. 
+                Some may say net art has much to do with networking art e.g., telecommunication, we can gather much that net art is artwork done for the net, created on the net for the net. 
+                To understand net art, “…it should speak in net language” (Lialina in Artsy). Net art is about creating art that will function on the internet, that will be engaged with through the internet (mostly). 
+                We have learned an example of an interactive net art by Olia Lialina called ‘My Boyfriend Came Back from the War’, 1996, and have understood how this story narrative challenges the 
+                imagination and the organization of memory to tell the rest of its net art story. Understanding how the Internet/World Wide Web has managed to create a new medium that lies in no usual sub-order or medium category 
+                has been interesting to learn; the power of technology, combined with browser and software elements and creativity can create a whole other form of work.
+                </p>
+                </div>
             </>
         </div>
     );
@@ -53,4 +146,7 @@ function About() {
 export default About;
 
 /*image sourced:Photo by Denys Gromov from Pexels:< https://www.pexels.com/photo/high-rise-buildings-in-dubai-at-night-7974827/>. Accessed: 29 May 2024.
-image sourced: Photo by Steve Johnson: <https://www.pexels.com/photo/abstract-painting-1400190/>.Accessed: 29 May 2024.*/
+image sourced: Photo by Steve Johnson: <https://www.pexels.com/photo/abstract-painting-1400190/>.Accessed: 29 May 2024.
+image sourced: Photo by Steve Johnson: <https://www.pexels.com/photo/pink-blue-green-and-yellow-abstract-painting-2007087/>. Accessed:29 May 2024.
+image sourced: TATE. Tate. Internet Art. Online. < https://www.tate.org.uk/art/art-terms/i/internet-art#:~:text=Internet%20art%20is%20art%20that,browser%20art%20and%20software%20art>. Date Accessed: 26 April 2024.
+image sourced: Photo by Steve Johnson: <https://www.pexels.com/photo/multicolored-abstract-painting-1509534/>.Accessed: 29 May 2024. */
