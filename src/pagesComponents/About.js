11d8f830f94d6bc395b536e8e8100948e37f5306
Keitu <125948549+Keitu7@users.noreply.github.com>
2024-05-29 13:27:28 +0200

    designPage

--- src/pagesComponents/About.js
+++ src/pagesComponents/About.js
@@ -5,9 +5,46 @@ function About() {
     return (
         <div className='home'>
             <div className='bkImage'></div>
+            <h1 className='header'>The Works</h1>
             <>
-                <h1 className='header'>The Works</h1>
-                
+                <h2>DESIGN</h2>
+                <div className='designbkImage'></div>
+                <div className='designborder'>
+                    <p className='design1'>The design process for the website was a unique journey, espcially considering the new library we had learnt this semester.
+                    I had to find the best suited theme to potray my new learnt skills and found that designing a portfolio would be better suited for me as learning and implementing all the learn knowledge of react was fairly overwhelming for me.
+                    Therefore, designing something familiar was a rather 'safer'option.
+                    Below are the wireframes, colour scheme and inspiration images/references.</p>
+                    <div className='w1'></div>
+                    <div className='w2'></div>
+                    <div className='w3'></div>
+                    <div className='w4'></div>
+                    <div className='w5'></div>
+                    <div className='w6'></div>
+                    <p className='design2'>
+                    Style guide:
+                    The colour palette explored and used throughout this website design makes use of tones of yellow, green, and a darker shade of blue. 
+                    This choice of colour scheme was an expression of my interest in more natural, calmer tones that spoke of a softer touch for the eye at first glance. 
+                    This site is a portfolio site which would mean essentially communicating who the designer is, my persona, and expression of personality. 
+                    The layout simply aims to guide the viewer through an uncomplicated user interface that represents and directly communicates my style of simplicity. 
+                    Inspired by a portfolio site by Shinya Okano, Shinya uses a simple interface that creates and allows for an uncomplicated smooth user experience of the interface elements throughout the site 
+                    – this helped me understand how to clearly communicate the simple User Interface that I wished to create without having bore a viewer with a too simplistic experience. 
+                    The layout is therefore straightforward, the website guides the user from the Homepage (white introduces the site designer) to the About page which leads to the design, theory, and internet art section, and lastly the Contact page. 
+                    The navigation bar and section links provide an easier lead through the page, and the site logo plays the Homepage link. The Font used also tries/aims to communicate the simple, calm, yet still interesting, eye-catching, 
+                    sophisticated natural organic theme the images, and colour scheme communicate. 
+                    </p>
+                    <p className='design3'>
+                    Rationale & Reflection on UI and UX Design Choices:
+                    The User Interface design in this design mainly focused on the colour scheme effect on first interaction with the eye. 
+                    I wanted to go for colours that communicated a calmer, natural/neutral yet still sophisticated feel, and since colour would essentially be the first element of a website that would meet the viewer's eye, 
+                    I wanted to draw in the user instantly. I do believe these colours, colour schemes and background images successfully communicated that. Although I would have liked to use much of the brighter colours of the selected colour scheme, in the site. 
+                    Reflecting on the user interactivity design, the site has made use of a simplistic interactive system, from the navigation bar to the footer to the information texts/headings. The site maintains the conventional flow of a homepage, about (information)page 
+                    and a contact page needed in any website no matter the theme. The site adheres to navigational links that are an established and still working convention in web design; the site does not seek to move things in a ‘creative way’ that would end up confusing the user.  
+                    I do, however, know that there is much yet to fix regarding the site's extent of interactivity, especially with what CSS and JavaScript enable a designer to do. I would have liked to add more dynamic movement to the site, to emphasize the sense of flow, smoothness and calm. 
+                    The User experience is about the feel the user feels, and experiences when interacting/ using the site.  This site’s simple design allows it to adhere to the conventional practices of “usable, equitable, able and useful” (Quyen Dao, 31 March, 2024). 
+                    Once again, with the addition of animated elements, movement and interactable pieces, the site would heighten the user experience by making it fun, attractive and overall an ‘communicative’ experience with the user. 
+
+                    </p>
+                </div>
             </>
         </div>
     );
@@ -15,4 +52,5 @@ function About() {
 
 export default About;
 
-//image sourced:Photo by Denys Gromov from Pexels:< https://www.pexels.com/photo/high-rise-buildings-in-dubai-at-night-7974827/>. Accessed: 29 May 2024.//
+/*image sourced:Photo by Denys Gromov from Pexels:< https://www.pexels.com/photo/high-rise-buildings-in-dubai-at-night-7974827/>. Accessed: 29 May 2024.
+image sourced: Photo by Steve Johnson: <https://www.pexels.com/photo/abstract-painting-1400190/>.Accessed: 29 May 2024.*/
