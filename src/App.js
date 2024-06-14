2ae502c4377f62df1e901599281764123e9a9c8d
Keitu <125948549+Keitu7@users.noreply.github.com>
2024-06-14 17:11:04 +0200

    fixing

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ function App() {
         <Navbar />
        <Routes>
          <Route path='/' element={<Home />} />
+         <Route path='/home' element={<Home />} />
          <Route path='/about' element={<About />} />
          <Route path='/internetart' element={<InternetArt />} />
          <Route path='/contact' element={<Contact />} />
